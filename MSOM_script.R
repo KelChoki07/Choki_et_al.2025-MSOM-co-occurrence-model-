279b35f2199e6290df2dd5b5cc63d047a763af11
Karma Choki <kelchoki7@gmail.com>
2025-08-28 17:23:58 -0400

    Update MSOM_script.R

--- MSOM_script.R
+++ MSOM_script.R
@@ -69,68 +69,7 @@ slope4km <- scale(cov$sloposFM_4KM)
 ele500m <- scale(cov$ele500)
 
 ######################################################################################################
-######################################################################################################
 
-# Check for spatial autocorrelation
-library(ncf)
-
-# Extract covariates (just to build a glm)
-elev <- scale(cov$ele500)
-settle <- scale(cov$setden_2KM)
-river <- scale(RivDen2km)
-forest <- scale(cov$TC2)
-slope <- scale(cov$sloposFM_2KM)
-prey <- scale(cov$bd_nmix)
-
-# We don't need temporal replicates to check spatial autocorrelation
-# So we convert detection history into a matrix and just get one column with det/non-det data
-
-# Convert into a matrix
-DHagc <- as.matrix(ch_asiaticgoldencat)
-
-# Get a single column detection/non-detection data for this analysis only
-det_agc <- apply(DHagc, 1, max, na.rm=T) #max means max value of index(?)
-View(det_agc)
-
-# Fit a logistic regression for naive occupancy on global model
-glm_agc  <- glm(det_agc ~ elev + I(elev^2) + settle + river + forest + slope + prey, family=binomial)
-
-# Spline correlogram for naive occupancy model
-# AGC
-correlog_agc <- spline.correlog(
-  x=cov[, 3],
-  y=cov[, 4],
-  z=residuals(glm_agc, type="pearson"), xmax=50,
-  latlon=T)
-plot(correlog_agc) 
-
-# Same for other two species
-# MC
-DHmc <- as.matrix(ch_marbledcat)
-det_mc <- apply(DHmc, 1, max, na.rm=T) 
-glm_mc  <- glm(det_mc ~ elev + I(elev^2) + settle + river + forest + slope + prey, family=binomial)
-correlog_mc <- spline.correlog(
-  x=cov[, 3],
-  y=cov[, 4],
-  z=residuals(glm_mc, type="pearson"), xmax=50,
-  latlon=T)
-plot(correlog_mc) 
-
-# LC
-DHlc <- as.matrix(ch_leopardcat)
-det_lc <- apply(DHlc, 1, max, na.rm=T) 
-glm_lc  <- glm(det_lc ~ elev + I(elev^2) + settle + river + forest + slope + prey, family=binomial)
-correlog_lc <- spline.correlog(
-  x=cov[, 3],
-  y=cov[, 4],
-  z=residuals(glm_lc, type="pearson"), xmax=50,
-  latlon=T)
-plot(correlog_lc) 
-
-# The CIs for correlation estimates on y-axis includes zero, this means there is no evidence
-# of the presence of autocorrelation for all the species
-
-######################################################################################################
 ######################################################################################################
 
 # Convert to data frame from matrix-array for covariates
@@ -205,51 +144,8 @@ confint(m26, type="det") # confidence interval for detection probability estimat
 confint(m26, type="state") # confidence interval for occupancy probability estimates
 
 ##########################################
-########### Plotting marginal occupancy probability#################################
 
-# We can use outputs from predict() to compare marginal occupancy across species with a plot. 
-sp1_marg <- predict(m26, type="state", species="goldencat") #predicts the marginal occupancy for all sites #Mention in paper
-MO_AGC <- c(mean(sp1_marg[,1]), mean(sp1_marg[,3]), mean(sp1_marg[,4]))
-sp1_marg_Det <- predict(m26, type="det", species="goldencat") #predicts the marginal occupancy for all sites
-Det_AGC <- c(mean(sp1_marg_Det[,1]), mean(sp1_marg_Det[,3]), mean(sp1_marg_Det[,4]))
-#0.2081516 0.1701973 0.2532498
-
-sp2_marg <- predict(m26, type="state", species="marbledcat")
-MO_MC <- c(mean(sp2_marg[,1]), mean(sp2_marg[,3]), mean(sp2_marg[,4])) #This line is important
-sp2_marg_Det <- predict(m26, type="det", species="marbledcat") #predicts the marginal occupancy for all sites
-Det_MC <- c(mean(sp2_marg_Det[,1]), mean(sp2_marg_Det[,3]), mean(sp2_marg_Det[,4]))
-#0.2357853 0.1879319 0.2980222
-
-sp3_marg <- predict(m26, type="state", species="leopardcat")
-MO_LC <- c(mean(sp3_marg[,1]), mean(sp3_marg[,3]), mean(sp3_marg[,4]))
-sp3_marg_Det <- predict(m26, type="det", species="leopardcat") #predicts the marginal occupancy for all sites
-Det_LC <- c(mean(sp3_marg_Det[,1]), mean(sp3_marg_Det[,3]), mean(sp3_marg_Det[,4]))
-#0.2390136 0.2031544 0.2807317
-
-# Combine all three species
-all_marg <- rbind(sp1_marg[1,], sp2_marg[1,], sp3_marg[1,])
-all_det <- rbind(sp1_marg_Det[1,], sp2_marg_Det[1,], sp3_marg_Det[1,])
-all_marg$Species <- c("Asiatic golden cat", "Marbled cat", "Leopard cat")
-all_det$Species <- c("Asiatic golden cat", "Marbled cat", "Leopard cat")
-all_marg$Type <- "Occupancy"
-all_det$Type <- "Detection"
-
-# Do the plot 
-k <- ggplot(data = tidy_est, aes(x = Type, y = Predicted, ymin = lower, ymax = upper, color = Species)) +
-  geom_pointrange(position = position_dodge(0.3)) +
-  scale_color_manual(values = c("#e9493b","#E7B800","#65b185"), name = NULL) +
-  ylab("Predicted probability") +
-  xlab("") +
-  theme_bw() +
-  theme(
-    legend.position = c(0.86, 0.9),
-    legend.background = element_blank(),
-    text = element_text(size = 15),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank()
-  )
-
-k
 
 ggsave("Marginal_occ_det_prob_all_sps_1.png", k, width=6.9, height=4.5)
 #######################################################################################################
+
